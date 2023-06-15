b01c8096c0c5fefa5f77a92ae03000b081ea8b48
Lorent <aliulorent@gmail.com>
2023-06-15 16:34:26 -0400

    Fixed conditional rendering when searching

--- app/DataGrid.jsx
+++ app/DataGrid.jsx
@@ -20,13 +20,28 @@ function DataGrid({pokemon}) {
         console.log(visibleList);
     }, [searchTerm]);
 
+    let displayRender;
+    if (searchTerm.length > 0 && searchList.length === 0){
+        // CONDITION: There is a search term but it cannot be found! Empty list!
+        displayRender = <h2>{"Could not find search results!"}</h2>;
+    }
+    else if (searchTerm.length > 0 && searchList.length > 0 ){
+        // CONDITION: Search term has been found! Return a list of results!
+        displayRender = searchList.map(p =>(<Card pokemon={p} key={p.id}/>));
+    }
+    else if (searchTerm.length === 0) {
+        // CONDITION: Nothing is being searched, display visibleList
+        displayRender = visibleList.map(p =>(<Card pokemon={p} key={p.id}/>));
+    }
+
   return (
     <>
         <input value={searchTerm} placeholder='Search for a Pokemon!' onChange={(event)=>setSearchTerm(event.target.value)}/>
         <div className='grid-container grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
             {/* {visibleList.map(p =>(<Card pokemon={p} key={p.id}/>))} */}
             {/* {searchTerm.length > 0 ? (fullList.filter(p=>p.name.includes(searchTerm)).map(i=>(<Card pokemon={p}/>))): visibleList.map(p =>(<Card pokemon={p}/>)) } */}
-            { searchList.length > 0 ? searchList.map(p =>(<Card pokemon={p} key={p.id}/>)) : visibleList.map(p =>(<Card pokemon={p} key={p.id}/>))}
+            {/* { searchList.length > 0 ? searchList.map(p =>(<Card pokemon={p} key={p.id}/>)) : visibleList.map(p =>(<Card pokemon={p} key={p.id}/>))} */}
+            {displayRender}
         </div>
     </>
   )
